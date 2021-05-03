1384a43f6ae35e736cb3f7691e5f8468e8528f9b
Cbeterous <celine.beterous@gmail.com>
2021-05-03 15:12:34 +0200

    titre pres

--- client/src/components/slideCreation/SlideCreation.tsx
+++ client/src/components/slideCreation/SlideCreation.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { FormEvent, useEffect, useState } from 'react'
 import { Editor } from '@tinymce/tinymce-react';
 import Slide from './Slide';
 import { SlideInterface } from './interfaces';
 import './SlideCreation.css'
 import Button from '../global/button/Button'
+import Input from '../global/input/Input';
 
 const SlideCreation = () => {
 
   const [ActiveContent, setActiveContent] = useState<string>('')
   const [slideList, setSlideList] = useState<SlideInterface[]>([{ content: '', isActive: true }])
+  const [titlePres, setTitlePres] = useState<string>('')
 
   const addSlide = () => {
     const slideListCopy = slideList.slice()
@@ -38,45 +40,58 @@ const SlideCreation = () => {
     }
   }
 
+  const save = () => {
+    console.log(slideList);
+    console.log(titlePres);
+  }
+
   return (
-    <div className="slideCreation-container">
-      <div className="editor-container">
-        <Editor
-          initialValue=""
-          value={ActiveContent}
-          init={{
-            height: 800,
-            menubar: true,
-            plugins: [
-              'advlist autolink lists link image charmap print preview anchor',
-              'searchreplace visualblocks code fullscreen',
-              'insertdatetime media table paste code help wordcount'
-            ],
-            toolbar:
-              'undo redo | formatselect | bold italic backcolor | \
-            alignleft aligncenter alignright alignjustify | \
-            bullist numlist outdent indent | removeformat | help'
-          }}
-          onEditorChange={content => {
-            const slideListCopy = slideList.slice()
-            slideListCopy.filter(slide => slide.isActive)[0].content = content;
-            setSlideList(slideListCopy)
-            setActiveContent(content)
-          }}
-        />
+    <div>
+      <div>
+        <Input type="text" placeholder="Titre de la présentation" onChange={(e: FormEvent<HTMLInputElement>) => setTitlePres(e.currentTarget.value)} />
+        <Button onClick={save}>Enregistrer</Button>
       </div>
-      <div className="slideCreation-rigth-container">
-        <div className="slides-container">
-          <h2>Slide show</h2>
-            {slideList.map((slide: SlideInterface, index) => {
-              return (
-                <Slide slide={slide} index={index} changeSlide={changeSlide} handleDelete={handleDelete} />
-              )
-            })}
+      <div className="slideCreation-container">
+      
+        <div className="editor-container">
+          <Editor
+            initialValue=""
+            value={ActiveContent}
+            init={{
+              height: 800,
+              menubar: true,
+              plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+              ],
+              toolbar:
+                'undo redo | formatselect | bold italic backcolor | \
+              alignleft aligncenter alignright alignjustify | \
+              bullist numlist outdent indent | removeformat | help'
+            }}
+            onEditorChange={content => {
+              const slideListCopy = slideList.slice()
+              slideListCopy.filter(slide => slide.isActive)[0].content = content;
+              setSlideList(slideListCopy)
+              setActiveContent(content)
+            }}
+          />
         </div>
-        <Button onClick={addSlide}>Ajouter</Button>
+        <div className="slideCreation-rigth-container">
+          <div className="slides-container">
+            <h2>Slide show</h2>
+              {slideList.map((slide: SlideInterface, index) => {
+                return (
+                  <Slide slide={slide} index={index} changeSlide={changeSlide} handleDelete={handleDelete} />
+                )
+              })}
+          </div>
+          <Button onClick={addSlide}>Ajouter</Button>
+        </div>
       </div>
     </div>
+    
   )
 }
 
