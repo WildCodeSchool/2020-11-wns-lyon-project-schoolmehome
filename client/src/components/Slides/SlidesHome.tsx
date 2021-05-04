5ff7a527ef14fc7819400df485cdd2bb765bddc6
Cbeterous <celine.beterous@gmail.com>
2021-05-04 14:56:50 +0200

    fix

--- client/src/components/Slides/SlidesHome.tsx
+++ client/src/components/Slides/SlidesHome.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { Card, CardContent, Grid } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Presentation } from '../slideCreation/interfaces';
@@ -10,15 +11,11 @@ export const SlidesHome = () => {
 
   const { loading, error, data } = useQuery(GET_PRESENTATIONS);
   const [presentation, setPresentation] = useState<Presentation[]>([])
-  const [selectedPres, setSelectedPres] = useState<string>();
-  const [activeSlide, setActiveSlide] = useState<number>(0)
-  const [visible, setVisible] = useState<boolean>(false);
 
   useEffect(() => {
     if (data) {
       if (data.findAllPresentation.length > 0) {
         setPresentation(data.findAllPresentation)
-        setSelectedPres(data.findAllPresentation[0]._id)
       }
     }
   }, [data])
@@ -26,11 +23,28 @@ export const SlidesHome = () => {
     return(
         <div>
             <Link to="/slides/creation" className="link">Créer un cour</Link>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="flex-start"
+              spacing={4}
+            >
             {
                 presentation.map( p => {
-                    return <div>{p.title}</div>
+                    return (
+                      <Grid item xs={2}>
+                        <Card elevation={3} className="blue" >
+                            <CardContent>
+                            {p.title}
+                          </CardContent>
+                        </Card>
+                      </Grid>
+                    )
                 })
-            }
+            }  
+            </Grid>
+            
         </div>
     )
 }
