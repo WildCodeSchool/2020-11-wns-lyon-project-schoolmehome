5d4bd2e307b97eaac95ce6c0d335717d85f12f1d
Cbeterous <celine.beterous@gmail.com>
2021-06-01 16:51:11 +0200

    dashboard éleve

--- client/src/components/dashboard/dashboard-teacher/DashboardTeacher.tsx
+++ client/src/components/dashboard/dashboard-teacher/DashboardTeacher.tsx
@@ -1,6 +1,6 @@
-import { CardActionArea, CardContent, CardMedia, createStyles, Typography } from '@material-ui/core';
-import { Grid, makeStyles, Card, Theme } from '@material-ui/core'
-import React, { useContext, useEffect, useState } from 'react'
+import { CardContent, CardMedia, Typography } from '@material-ui/core';
+import { Grid, Card } from '@material-ui/core'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './DashboardTeacher.css'
 import dashboardCours from '../../../image/dashboard-cours.png';
